6f7fa1b46a1234102b3a8583d60c497383de5866
Hisaalu <hisaaalujuniornelson@gmail.com>
2026-09-25 09:24:23 +0300

    configured dynamic paths

--- index.php
+++ index.php
@@ -5,7 +5,16 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once __DIR__ . '/constants.php';
+$rootDir = __DIR__;
+if (basename($rootDir) === 'public') {
+    $rootDir = dirname($rootDir);
+}
+
+if (!defined('ROOT_PATH')) {
+    define('ROOT_PATH', $rootDir);
+}
+
+require_once ROOT_PATH . '/constants.php';
 require_once ROOT_PATH . '/vendor/autoload.php';
 
 require_once APP_PATH . '/Core/Environment.php';
